3c51374364baa2bf5dd016e49ac64b8c7e8c6503
Katie Christensen <kac13@4rice.edu>
2018-10-22 21:54:44 -0500

    completed query/sentence practice problem

---
+++ src/com/company/QuerySentences.java
@@ -0,0 +1,38 @@
+package com.company;
+import java.util.*;
+import java.util.Collections;
+
+/**Given n sentences and q queries, for each query print the number of sentences that contains all the words of the query.**/
+public class QuerySentences {
+
+  private static void querySentences(List<String> sentences, List<String> queries) {
+
+    for (String query : queries) {
+      query = query.toLowerCase();
+      List<String> queryWords = Arrays.asList(query.split("[^\\w]"));
+      int numMatches = 0;
+
+      for (String sentence : sentences) {
+        sentence = sentence.toLowerCase();
+        List<String> stringWords = Arrays.asList(sentence.split("[^\\w]"));
+
+        if (stringWords.containsAll(queryWords)){
+          numMatches ++;
+        }
+      }
+
+      System.out.println("The query \"" + query + "\"  has " + numMatches + " sentence(s) that contain all of its words.");
+
+    }
+  }
+
+  public static void main(String[] args) {
+
+    List<String> sentences = Arrays.asList("The cat was bad.", "The dog was good.", "This is good and bad.");
+    List<String> queries = Arrays.asList("cat bad" , "the" , "was" , "good bad");
+
+    querySentences(sentences, queries);
+
+  }
+}
+
